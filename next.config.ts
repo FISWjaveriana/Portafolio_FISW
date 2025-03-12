6451b3fc9df39fef816d4a6c66001c31d4fb8214
DereKk8 <dereksarmiento@javeriana.edu.co>
2025-03-12 11:25:29 -0500

    inicial

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Enables static exports
+  basePath: process.env.NODE_ENV === 'production' ? '/my-nextjs-typescript-app' : '',
+  images: {
+    unoptimized: true, // For GitHub Pages compatibility
+  },
 };
 
 export default nextConfig;
